3160a8f2060fb8b45a1784fcb1b224671362c5dd
Lindsey Kuper <lindsey@composition.al>
2017-02-20 18:28:00 -0800

    Tweaks to montage image.

--- images/create_montage.sh
+++ images/create_montage.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+# A script to generate the montage background image used in our 2017
+# website.  Depends on ImageMagick and coreutils (gshuf).
+
 # Create temporary directory for images
-mkdir -p /tmp/bangbangcon_images/resized/
+mkdir -p /tmp/bangbangcon_images/resized
+mkdir /tmp/bangbangcon_images/final
 
 # Copy speaker images to temp dir, in an order such that if a given
 # speaker occurs more than once, newer images will overwrite older
@@ -11,19 +15,38 @@ cp ../2015/images/speakers/*.png /tmp/bangbangcon_images/
 cp ../2016/images/speakers/*.png /tmp/bangbangcon_images/
 
 # Convert images to 50x50px
-for image in `ls /tmp/bangbangcon_images/`; do
-    convert /tmp/bangbangcon_images/$image -resize 50x50 /tmp/bangbangcon_images/resized/$image
+i=1
+for image in `ls /tmp/bangbangcon_images/*.png`; do
+    convert $image -resize 50x50 /tmp/bangbangcon_images/resized/$i.png
+    let "i++";
 done
 
-# Make 4 copies of each (so we have original + 4) -- determined by experimentation
-for image in `ls /tmp/bangbangcon_images/resized/`; do
-    for ((i=1; i<=4; i++)); do
-        cp /tmp/bangbangcon_images/resized/$image /tmp/bangbangcon_images/resized/$i$image
+# Numbers determined by experimentation -- this is what looked good!
+# If the number of source images changes, these will likely have to
+# change too.
+COPIES=5
+GRID_HEIGHT=9
+LEFTOVER_SLOTS=7 # number of spaces left over in grid
+
+# Make copies and order images randomly
+j=1
+for ((i=1; i<=$COPIES; i++)); do
+    for image in `ls /tmp/bangbangcon_images/resized/*.png | gshuf`; do
+        cp $image /tmp/bangbangcon_images/final/$j.png;
+        let "j++";
     done
 done
 
-# Arrange in a grid 9 images tall -- determined by experimentation
-montage /tmp/bangbangcon_images/resized/*.png -tile x9 -geometry +0+0 montage.png
+# Pick a random 7 images to fill in the gap at the end
+k=1
+for image in `ls /tmp/bangbangcon_images/final/*.png | gshuf | head -n $LEFTOVER_SLOTS`; do
+    #echo $image;
+    cp $image /tmp/bangbangcon_images/final/extra_$k.png;
+    let "k++";
+done
+
+# Arrange in a grid
+montage /tmp/bangbangcon_images/final/*.png -tile x$GRID_HEIGHT -geometry +0+0 -quality 90 montage.jpg
 
 # Get rid of temp files
 rm -r /tmp/bangbangcon_images/
