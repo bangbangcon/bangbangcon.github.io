bf13bb94112aae4773ccf6edffa2bfe829741492
Sarah Withee <2601974+geekygirlsarah@users.noreply.github.com>
2022-03-25 22:35:02 -0400

    Add Dockerized montage creator, modify script
    Tidy up montage creator script, rename gshuf to shuf, have it output number of speakers from the past

--- images/create_montage.sh
+++ images/create_montage.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
 
 # A script to generate the montage background image used in our 2019
-# website.  Depends on ImageMagick and coreutils (gshuf).
+# website.  Depends on ImageMagick and coreutils (shuf).
 
 # Create temporary directory for images
-mkdir -p /tmp/bangbangcon_images/resized
-mkdir /tmp/bangbangcon_images/final
+mkdir -p /tmp/bangbangcon_images/
 
 # Copy speaker images to temp dir, in an order such that if a given
 # speaker occurs more than once, newer images will overwrite older
 # ones
-cp ../2014/images/speakers/*.png /tmp/bangbangcon_images/
-cp ../2015/images/speakers/*.png /tmp/bangbangcon_images/
-cp ../2016/images/speakers/*.png /tmp/bangbangcon_images/
-cp ../2017/images/speakers/*.png /tmp/bangbangcon_images/
-cp ../2018/images/speakers/*.png /tmp/bangbangcon_images/
-cp ../2019/images/speakers/*.png /tmp/bangbangcon_images/
-cp ../2020/images/speakers/*.png /tmp/bangbangcon_images/
+# -u to always update a speaker's photo
+cp -u ../*/images/speakers/*.png /tmp/bangbangcon_images/
+
+speakers=`ls /tmp/bangbangcon_images/ | wc -l`
+echo
+echo "There are $speakers speakers from past !!Cons!"
+echo
+
+# Create temporary directories for manipulated images
+mkdir /tmp/bangbangcon_images/resized
+mkdir /tmp/bangbangcon_images/final
 
 # Convert images to 50x50px
 i=1
@@ -29,13 +32,13 @@ done
 # If the number of source images changes, these will likely have to
 # change too.
 COPIES=3
-GRID_HEIGHT=12
+GRID_HEIGHT=15
 LEFTOVER_SLOTS=12 # number of spaces left over in grid
 
 # Make copies and order images randomly
 j=1
 for ((i=1; i<=$COPIES; i++)); do
-    for image in `ls /tmp/bangbangcon_images/resized/*.png | gshuf`; do
+    for image in `ls /tmp/bangbangcon_images/resized/*.png | shuf`; do
         cp $image /tmp/bangbangcon_images/final/$j.png;
         let "j++";
     done
@@ -43,7 +46,7 @@ done
 
 # Pick random images to fill in the gap at the end
 k=1
-for image in `ls /tmp/bangbangcon_images/final/*.png | gshuf | head -n $LEFTOVER_SLOTS`; do
+for image in `ls /tmp/bangbangcon_images/final/*.png | shuf | head -n $LEFTOVER_SLOTS`; do
     #echo $image;
     cp $image /tmp/bangbangcon_images/final/extra_$k.png;
     let "k++";
